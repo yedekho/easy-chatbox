a1180278938c195b114948167381c220caa84b9c
yedekho <143316647+yedekho@users.noreply.github.com>
2024-12-14 15:56:26 +0530

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'YOUR_PROJECT_URL'; // Replace with your Project URL
-const supabaseAnonKey = 'YOUR_ANON_KEY'; // Replace with your anon key
+const supabaseUrl = 'https://gidnjcjahnkrhakjjwvg.supabase.co'; // Replace with your Project URL
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdpZG5qY2phaG5rcmhha2pqd3ZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQxNTc4NDAsImV4cCI6MjA0OTczMzg0MH0.BK2JBsRpuXNSwK9XFE1w9n1jzCT0arWEtLK-1fLcpNI'; // Replace with your anon key
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
